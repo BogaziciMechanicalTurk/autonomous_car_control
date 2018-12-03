b1e7a4533e1002ab797945db8e74d65c2d956fb4
2011401285 <kadir.turksoy@boun.edu.tr>
2018-12-03 18:06:22 +0300

    clean-up arduino codes

--- arduino/CarControl_keyop/CarControl_keyop.ino
+++ arduino/CarControl_keyop/CarControl_keyop.ino
@@ -3,12 +3,20 @@
  * Prints "hello world!"
  */
 
-const int stepPin = 5; 
-const int dirPin = 2; 
-const int enPin = 8;
-const int fwPin = 11;
-//const int potPin='A2';
-int potValPrev=0;
+//const int stepPin = 5; 
+//const int dirPin = 2; 
+//const int enPin = 9;
+
+//const int fwPin = 11;
+//const int brPin = 3;
+
+const int stepPin = 9; 
+const int dirPin = 10; 
+const int enPin = 11;
+
+const int fwPin = 2; 
+const int brPin = 5;
+
 
 #include <ros.h>
 #include <std_msgs/Int16.h>
@@ -16,30 +24,12 @@ int potValPrev=0;
 
 #include "stdlib.h"
 
-int pp0 = 0;  // analog pin used to connect the potentiometer
-int pp1 = 1;  // analog pin used to connect the potentiometer
-int pp2 = 2;  // analog pin used to connect the potentiometer
-
-double a1 =       50.26;
-double b1 =    0.001145;
-double c1 =      -53.15;
-double d1 =    -0.04186;
-
-double a2 =   -7.83e-13;
-double b2 =     0.03076;
-double c2 =       116.3;
-double d2 =   -0.001007;
-
-double l_max = 157;
-
-int val;    // variable to read the value from the analog pin
-double d_val;
-double result;
 
 ros::NodeHandle  nh;
 
 int turn = 0;
 int forward = 0;
+int brake = 0;
 
 void messageCb( const std_msgs::Int16& keyboard_msg){
   turn = keyboard_msg.data;
@@ -49,8 +39,12 @@ void messageCb1( const std_msgs::Int16& keyboard_msg){
   forward = keyboard_msg.data;
 }
 
+void messageCb2( const std_msgs::Int16& keyboard_msg){
+  brake = keyboard_msg.data;
+}
 ros::Subscriber<std_msgs::Int16> sub("turn", &messageCb);
 ros::Subscriber<std_msgs::Int16> sub1("forward", &messageCb1);
+ros::Subscriber<std_msgs::Int16> sub2("brake", &messageCb2);
 
 std_msgs::Float32 steer_msg;
 ros::Publisher steerPub("steer_fb", &steer_msg);
@@ -66,21 +60,21 @@ void setup()
   pinMode(dirPin,OUTPUT);
   pinMode(enPin,OUTPUT);
   pinMode(fwPin, OUTPUT);
+  pinMode(brPin, OUTPUT);
   digitalWrite(enPin,LOW);
+  digitalWrite(brPin,LOW);
 }
 
 void loop()
 {
-
-  //int potVal = analogRead(A2);
 
   if(forward == 1){
       analogWrite(LED_BUILTIN,1.5*255/5);
       analogWrite(fwPin,1.9*255/5);
   }
   else{
-      analogWrite(LED_BUILTIN,0.5*255/5);
-      analogWrite(fwPin,0.5*255/5);   
+      analogWrite(LED_BUILTIN,0.0*255/5);
+      analogWrite(fwPin,0.0*255/5);   
   }
   
 
@@ -99,8 +93,16 @@ void loop()
       delayMicroseconds(500);
    }
 
+   
+  if(brake == 1){
+      digitalWrite(brPin,LOW);
+  }
+  else{
+      digitalWrite(brPin,HIGH);  
+  }
 
-  delayMicroseconds(4000);
+
+  delayMicroseconds(3000);
 
   nh.spinOnce();
 
